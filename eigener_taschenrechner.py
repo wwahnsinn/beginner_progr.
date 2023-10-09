636400220537f3fcc62494e2c042961ccc2b42f1
wwahnsinn <137478272+wwahnsinn@users.noreply.github.com>
2023-10-09 21:30:19 +0200

    Update eigener_taschenrechner.py

--- eigener_taschenrechner.py
+++ eigener_taschenrechner.py
@@ -1,44 +1,52 @@
-print("Dies ist ein Taschenrechner")
-print("Welche Rechnung möchten Sie durchführen? ")
-print("")
+while True:
 
-print("Addition                 [1]")
-print("Subtraktion              [2]")
-print("Multiplikation           [3]")
-print("Division                 [4]")
-print("")
+        print("Dies ist ein Taschenrechner")
 
-auswahl = int(input("Eingabe bitte ")) 
+        while True:
 
-if auswahl == 1:
-        print("Addition")
-elif auswahl == 2:
-        print("Subtraktion")
-elif auswahl == 3:
-        print("Multiplikation")
-elif auswahl == 4:
-        print("Division")
-else:
-        print("Falsche Eingabe")
+                print("Welche Rechnung möchten Sie durchführen? ")
+                print("")
+
+                print("Addition                 [1]")
+                print("Subtraktion              [2]")
+                print("Multiplikation           [3]")
+                print("Division                 [4]")
+                print("Rechner verlassen        [0]")
+                print("")
+
+                auswahl = int(input("Eingabe bitte ")) 
+
+                if auswahl == 1:
+                        print("Addition")
+                elif auswahl == 2:
+                        print("Subtraktion")
+                elif auswahl == 3:
+                        print("Multiplikation")
+                elif auswahl == 4:
+                        print("Division")
+                elif auswahl == 0:
+                        print("Rechner verlassen")
+                        exit()     
+                else:
+                        print("Falsche Eingabe")
+                        break
+
+                zahl_eins = int(input(" Bitte geben Sie die ERSTE Zahl ein "))
+                print(zahl_eins)
+
+                zahl_zwei = int(input(" Bitte geben Sie die ZWEITE Zahl ein "))
+                print(zahl_zwei)
 
 
-zahl_eins = int(input(" Bitte geben Sie die ERSTE Zahl ein "))
-print(zahl_eins)
 
-zahl_zwei = int(input(" Bitte geben Sie die ZWEITE Zahl ein "))
-print(zahl_zwei)
-
-
-
-if auswahl == 1:
-        print("Ergebnis: ", zahl_eins + zahl_zwei)
-elif auswahl == 2:
-        print("Ergebnis: ", zahl_eins - zahl_zwei)
-elif auswahl == 3:
-        print("Ergebnis: ", zahl_eins * zahl_zwei)
-elif auswahl == 4:
-        print("Ergebnis: ", zahl_eins / zahl_zwei)
-        print("Ergebnis: ", zahl_eins / zahl_zwei)
-else:
-        print("Ungültige Eingabe")
-
+                if auswahl == 1:
+                        print("Ergebnis: ", zahl_eins + zahl_zwei)
+                elif auswahl == 2:
+                        print("Ergebnis: ", zahl_eins - zahl_zwei)
+                elif auswahl == 3:
+                        print("Ergebnis: ", zahl_eins * zahl_zwei)
+                elif auswahl == 4:
+                        print("Ergebnis: ", zahl_eins / zahl_zwei)
+                else:
+                        print("Ungültige Eingabe")
+                        break
